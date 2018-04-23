3e9fdf9b12e2a99edc1107f38001c19e92bcdb72
mcy <201102430@cs-cnu.org>
2018-04-23 16:35:56 +0900

    modify title and adjust background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,28 +6,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  arr = ['.path-0','.path-1', '.path-2', '.path-3', '.path-4', '.path-5', '.path-6', '.path-7', '.path-8']
-  load_sketch_animation = function (s) {
-    var path = document.querySelector(s);
-    if(path){
-      var length = path.getTotalLength();
-      // Clear any previous transition
-      path.style.transition = path.style.WebkitTransition =
-        'none';
-      // Set up the starting positions
-      path.style.strokeDasharray = length + ' ' + length;
-      path.style.strokeDashoffset = length;
-      // Trigger a layout so styles are calculated & the browser
-      // picks up the starting position before animating
-      path.getBoundingClientRect();
-      // Define our transition
-      path.style.transition = path.style.WebkitTransition =
-        'stroke-dashoffset 2s ease-in-out';
-      // Go!
-      path.style.strokeDashoffset = '0';
-    }
-  }
-  ngAfterViewInit() {
-    this.arr.forEach(d => this.load_sketch_animation(d));
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
