3cd7972674020bf03df9676f1922b4cb22e49704
mcy <201102430@cs-cnu.org>
2018-04-26 12:46:22 +0900

    button event

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  HostBinding
+} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  group
+} from '@angular/animations';
+import { Router } from '@angular/router';
+import { RouteAnimation }   from '../animations';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
+  animations: [
+    RouteAnimation
+]
 })
-export class MainPageComponent implements OnInit {
-
-  constructor() { }
-
+export class MainPageComponent {
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display')   display = 'block';
+  @HostBinding('style.position')  position = 'absolute';
   arr = ['.path-0','.path-1', '.path-2', '.path-3', '.path-4', '.path-5', '.path-6', '.path-7', '.path-8', '.path-9']
+
+  constructor(
+    private router: Router
+  ) { }
+
+
   load_sketch_animation = function (s) {
     var path = document.querySelector(s);
     if(path){
@@ -26,15 +48,16 @@ export class MainPageComponent implements OnInit {
       path.getBoundingClientRect();
       // Define our transition
       path.style.transition = path.style.WebkitTransition =
-        'stroke-dashoffset 2s ease-in-out';
+        'stroke-dashoffset 2s ease';
       // Go!
       path.style.strokeDashoffset = '0';
+      // setTimeout(()=>{path.style.strokeDashoffset = length; console.log('occur')}, 2000);
     }
+  }
+  transitionTest(path) {
+    this.router.navigateByUrl(path);
   }
   ngAfterViewInit() {
     this.arr.forEach(d => this.load_sketch_animation(d));
-  }
-  ngOnInit(){
-
   }
 }
