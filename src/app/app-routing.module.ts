16f7b1344cb73e7c218bbde2b394952e24290559
mcy <201102430@cs-cnu.org>
2018-04-26 12:37:09 +0900

    add contactus projects route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AboutUsComponent } from './about-us/about-us.component'
+import { ProjectsComponent } from './projects/projects.component'
+import { ContactUsComponent } from './contact-us/contact-us.component'
 
 const routes: Routes = [
-  { path: '', component: MainPageComponent }
+  { path: '', component: MainPageComponent },
+  { path: 'aboutus', component: AboutUsComponent },
+  { path: 'contactus', component: ContactUsComponent },
+  { path: 'projects', component: ProjectsComponent }
 ];
 
 @NgModule({
